9a435310d341b1766d59729628ec82444db27ea1
Keith Pickford <Keith@FunctionZero.com>
2021-10-19 18:30:25 +0100

    Removed test code used to track down zBind problem when dealing with null values. Updated to MvvmZero 2.2.4 to fix problem with z:Bind.

--- SpaceDonkey/SpaceDonkey/Mvvm/Pages/HomePage.xaml.cs
+++ SpaceDonkey/SpaceDonkey/Mvvm/Pages/HomePage.xaml.cs
@@ -16,19 +16,6 @@ namespace SpaceDonkey.Mvvm.Pages
         public HomePage()
         {
             InitializeComponent();
-
-            Test2.PropertyChanged += Test_PropertyChanged;
-        }
-
-        private void Test_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            if(e.PropertyName == "IsVisible")
-            {
-                Debug.WriteLine($"GOTCHA! {Test2.IsVisible}");
-            }
-            else
-                Debug.WriteLine(e.PropertyName);
-
         }
     }
 }
